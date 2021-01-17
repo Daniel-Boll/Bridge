78570bfb580bc5991aab5b90fe4e4e28cd6e2d15
Daniel Boll <danielboll.academico@gmail.com>
2021-01-17 13:26:25 -0300

    feat(page): completing profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,10 +1,90 @@
+// Modules
+import { useState } from "react";
+
+// Components
+import Navigation from "../../components/Navigation";
+import RoadTimeline from "../../components/RoadTimeline";
+import DoughnutChart from "../../components/DoughnutChart";
+
+// Images
+import userImage from "../../images/userImage.png";
+
+// Styles
+import styles from "../../styles/Profile.module.css";
+import { ChevronDown } from "react-feather";
+
 const profilePage = () => {
-    return (
-      <>
-        <p>Profile Page</p>
-      </>
-    );
+  const [current, setCurrent] = useState("road");
+
+  const handleStatistics = () => {
+    setCurrent("statistic");
   };
-  
-  export default profilePage;
-  
+
+  const handleRoad = () => {
+    setCurrent("road");
+  };
+
+  return (
+    <>
+      <main className={styles.mainContainer}>
+        {/* Header */}
+        <div className={styles.switchButtonsContainer}>
+          <button
+            onClick={handleStatistics}
+            className={`${styles.switchButtons}
+           ${current == "statistic" ? styles.selected : ""}`}
+          >
+            Estatísticas
+          </button>
+          <button
+            onClick={handleRoad}
+            className={`${styles.switchButtons}
+           ${current == "road" ? styles.selected : ""}`}
+          >
+            Trilha
+          </button>
+        </div>
+
+        {current == "statistic" ? <StatisticPage /> : <RoadPage />}
+
+        <Navigation active="profile" />
+      </main>
+    </>
+  );
+};
+
+const StatisticPage = () => {
+  return (
+    <>
+      <section className={styles.profileContainer}>
+        <div className={styles.profileCard}>
+          <h1 className={styles.userName}>Felipi Lima</h1>
+          <img
+            className={styles.userImage}
+            src={userImage}
+            alt="Nome do usuário"
+          />
+          <p class={styles.userArea}>Tecnologia</p>
+        </div>
+      </section>
+
+      <section className={styles.statisticsContainer}>
+        <DoughnutChart />
+      </section>
+    </>
+  );
+};
+
+const RoadPage = () => {
+  return (
+    <>
+      <div className={styles.switch}>
+        <p className={styles.switchText}>Algoritmos</p>
+        <ChevronDown color={"#cacaca"} size={16} />
+      </div>
+      <RoadTimeline colors={["primary", "primary", "secondary", "secondary"]} />
+    </>
+  );
+};
+
+export default profilePage;
